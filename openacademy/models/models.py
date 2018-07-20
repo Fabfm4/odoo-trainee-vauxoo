cad2a3a5293f35f46f438aee46c5ea9cdbb8d9cd
Fabfm4 <favian.flone92@gmail.com>
2018-07-20 13:04:03 -0500

    [REF] constraints

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 
 def get_uid(self, *a):
@@ -19,6 +19,30 @@ class Course(models.Model):
         default=get_uid
     )
     sessions_id = fields.One2many("openacademy.session", "course_id")
+
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+        copied_count = self.search_count(
+            [("name", "=like", u"Copy of {}".format(self.name))]
+        )
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default["name"] = new_name
+        return super(Course, self).copy(default)
+
+    _sql_constraints = [
+        ("name_description_check",
+         "CHECK(name != description)",
+         "The title of the course should not be the description"),
+
+        ("name_unique",
+         "UNIQUE(name)",
+         "The course title must be unique"),
+    ]
 
 
 class Session(models.Model):
@@ -52,3 +76,31 @@ class Session(models.Model):
     def _taken_seats(self):
         for record in self.filtered(lambda r: r.seats):
             record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
+
+    @api.onchange("seats", "attendee_ids")
+    def _verify_valid_seats(self):
+        if self.filtered(lambda r: r.seats < 0):
+            self.active = False
+            return {
+                "warning": {
+                    "title": "Incorrect 'seats' value",
+                    "message": "The number of available seats may not be negative"
+                }
+            }
+        if self.seats < len(self.attendee_ids):
+            self.active = False
+            return {
+                "warning": {
+                    "title": "Too many attendees",
+                    "message": "Increase seats or remove excess attendees"
+                }
+            }
+        self.active = True
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError(
+                    "A session's instructor can't be an attendee"
+                )
