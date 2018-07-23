631c4e92339fde9a24b71bd8ba29cac6fb535b8b
Fabfm4 <favian.flone92@gmail.com>
2018-07-23 11:21:50 -0500

    [REF] Advanced views

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
 from odoo import models, fields, api, exceptions
 
 
@@ -53,6 +54,9 @@ class Session(models.Model):
     datetime_test = fields.Datetime(default=fields.Datetime.now)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
+    color = fields.Float()
+
     instructor_id = fields.Many2one(
         "res.partner",
         string="Instructor",
@@ -70,7 +74,23 @@ class Session(models.Model):
     )
     attendee_ids = fields.Many2many("res.partner", string="Attendees")
     taken_seats = fields.Float(compute="_taken_seats")
-    active = fields.Boolean(default=True)
+    end_date = fields.Date(
+        string="End Date",
+        store=True,
+        compute="_get_end_date",
+        inverse='_set_end_date',
+    )
+
+    attendees_count = fields.Integer(
+        string="Attendees count",
+        compute="_get_attendees_count",
+        store=True
+    )
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
 
     @api.depends("seats", "attendee_ids")
     def _taken_seats(self):
@@ -96,6 +116,22 @@ class Session(models.Model):
                 }
             }
         self.active = True
+
+    @api.depends("start_date", "duration")
+    def _get_end_date(self):
+        for r in self.filtered('start_date'):
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
 
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
