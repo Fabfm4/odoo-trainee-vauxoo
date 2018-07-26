88ec71c60188ab0e844013c46c407e21c2340ae6
Fabfm4 <favian.flone92@gmail.com>
2018-07-26 12:08:40 -0500

    [REF] openacademy: Add duplicate test to openacademy course

--- openacademy/tests/test_global_openadademy_course.py
+++ openacademy/tests/test_global_openadademy_course.py
@@ -1,7 +1,18 @@
 # -*- encoding: utf-8 -*-
 from psycopg2 import IntegrityError
 from odoo.tests.common import TransactionCase
-from odoo.tools import mute_logger
+from odoo.tools import mute_logger  # noqa
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 class GlobalTestOpenAcademyCourse(TransactionCase):
@@ -47,10 +58,18 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
         test_02_two_course_same_name_correct = False
         try:
             new_id = self.create_course('test1', 'test_description', None)
-            print("new id: ", new_id)
+            print(bcolors.OKBLUE, "new id: ", new_id, bcolors.ENDC)
             new_id2 = self.create_course('test1', 'test_description', None)
-            print("new id2: ", new_id2)
+            print(bcolors.OKBLUE, "new id2: ", new_id2, bcolors.ENDC)
         except IntegrityError:
             test_02_two_course_same_name_correct = True
 
         self.assertTrue(test_02_two_course_same_name_correct)
+
+    def test_03_duplicate_course(self):
+        """
+        Test to duplicate a course and check that work fine!
+        """
+        course = self.env.ref('openacademy.course0')
+        course_id = course.copy()
+        print(bcolors.OKBLUE, "copy course id: ", course_id, bcolors.ENDC)
